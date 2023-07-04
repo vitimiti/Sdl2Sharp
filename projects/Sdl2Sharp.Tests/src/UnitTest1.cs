775f82051e5b5cbeb2e71c9ef997ac104cd04b21
Victor Matia <vmatir@gmail.com>
2023-07-04 06:37:30 +0100

    Added required projects and runtimes.
    Added the required projects and all the native runtimes required for the
    library.
    
    Signed-off-by: Victor Matia <vmatir@gmail.com>

---
+++ projects/Sdl2Sharp.Tests/src/UnitTest1.cs
@@ -0,0 +1,17 @@
+using NUnit.Framework;
+
+namespace Sdl2Sharp.Tests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
---
+++ projects/Sdl2Sharp/src/Interop/Error.cs
@@ -0,0 +1,5 @@
+namespace Sdl2Sharp.Interop;
+
+internal static partial class Sdl
+{
+}
