9461037f347d79cdd70ab2cc2a24b817f1448127
Victor Matia <vmatir@gmail.com>
2023-07-08 23:05:48 +0100

    Added missing calling conventions.
    Added the calling conventions where they were missing.
    
    Signed-off-by: Victor Matia <vmatir@gmail.com>

--- projects/Sdl2Sharp/src/Interop/Clipboard.cs
+++ projects/Sdl2Sharp/src/Interop/Clipboard.cs
@@ -6,18 +6,20 @@ namespace Sdl2Sharp.Interop;
 
 internal static partial class Sdl
 {
-    [DllImport(LibraryName, EntryPoint = "SDL_SetClipboardText", CharSet = CharSet.Ansi)]
+    [DllImport(LibraryName, EntryPoint = "SDL_SetClipboardText", CallingConvention = CallingConvention.Cdecl,
+        CharSet = CharSet.Ansi)]
     public static extern int SetClipboardText(
         [MarshalAs(UnmanagedType.CustomMarshaler, MarshalTypeRef = typeof(SdlStringCustomMarshaler))]
         string text);
 
-    [DllImport(LibraryName, EntryPoint = "SDL_GetClipboardText", CharSet = CharSet.Ansi)]
+    [DllImport(LibraryName, EntryPoint = "SDL_GetClipboardText", CallingConvention = CallingConvention.Cdecl,
+        CharSet = CharSet.Ansi)]
     [return:
         MarshalAs(UnmanagedType.CustomMarshaler, MarshalTypeRef = typeof(SdlStringCustomMarshaler),
             MarshalCookie = SdlStringCustomMarshaler.Cookies.LeaveAllocated)]
     public static extern string GetClipboardText();
 
-    [DllImport(LibraryName, EntryPoint = "SDL_HasClipboardText")]
+    [DllImport(LibraryName, EntryPoint = "SDL_HasClipboardText", CallingConvention = CallingConvention.Cdecl)]
     [return: MarshalAs(UnmanagedType.I1)]
     public static extern bool HasClipboardText();
 }
