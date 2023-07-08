a7e8e2ad27556124e3e859f886e2f5379b093a9c
Victor Matia <vmatir@gmail.com>
2023-07-08 23:51:42 +0100

    Added more information.
    Added the CPU system information to the first example.
    
    Signed-off-by: Victor Matia <vmatir@gmail.com>

--- projects/Sdl2Sharp.Examples/Sdl2Sharp.Examples.Example001.HelloSdl/src/Program.cs
+++ projects/Sdl2Sharp.Examples/Sdl2Sharp.Examples.Example001.HelloSdl/src/Program.cs
@@ -4,6 +4,7 @@ using Sdl2Sharp.Utilities.Log;
 
 Functions.SetLogPriority((int)MyCategories.LibraryInformation, Priority.Information);
 Functions.SetLogPriority((int)MyCategories.SubsystemInformation, Priority.Information);
+Functions.SetLogPriority((int)MyCategories.CpuInformation, Priority.Information);
 
 Functions.SetLogOutputFunction((data, category, priority, message) =>
 {
@@ -41,7 +42,25 @@ Functions.SetLogOutputFunction((data, category, priority, message) =>
 }, DateTime.Now);
 
 Functions.LogInformation((int)MyCategories.LibraryInformation,
-    $"Running SDL v{NativeDllInformation.Version} [{NativeDllInformation.Revision}] on {Platform.Name}");
+    $"Using SDL v{NativeDllInformation.Version} [{NativeDllInformation.Revision}] on {Platform.Name}");
+
+Functions.LogInformation((int)MyCategories.CpuInformation, $"Logical Cores: {CpuInformation.LogicalCores}");
+Functions.LogInformation((int)MyCategories.CpuInformation, $"Cache Line Size: {CpuInformation.CacheLineSize}");
+Functions.LogInformation((int)MyCategories.CpuInformation, $"System RAM: {CpuInformation.SystemRam}MB");
+Functions.LogInformation((int)MyCategories.CpuInformation, $"CPU has the RDTSC instruction: {CpuInformation.HasRdtsc}");
+Functions.LogInformation((int)MyCategories.CpuInformation, $"CPU has AltiVec features: {CpuInformation.HasAltiVec}");
+Functions.LogInformation((int)MyCategories.CpuInformation, $"CPU has MMX features: {CpuInformation.HasMmx}");
+Functions.LogInformation((int)MyCategories.CpuInformation, $"CPU has 3DNow! features: {CpuInformation.Has3DNow}");
+Functions.LogInformation((int)MyCategories.CpuInformation, $"CPU has SSE features: {CpuInformation.HasSse}");
+Functions.LogInformation((int)MyCategories.CpuInformation, $"CPU has SSE2 features: {CpuInformation.HasSse2}");
+Functions.LogInformation((int)MyCategories.CpuInformation, $"CPU has SSE3 features: {CpuInformation.HasSse3}");
+Functions.LogInformation((int)MyCategories.CpuInformation, $"CPU has SSE4.1 features: {CpuInformation.HasSse41}");
+Functions.LogInformation((int)MyCategories.CpuInformation, $"CPU has SSE4.2 features: {CpuInformation.HasSse42}");
+Functions.LogInformation((int)MyCategories.CpuInformation, $"CPU has AVX features: {CpuInformation.HasAvx}");
+Functions.LogInformation((int)MyCategories.CpuInformation, $"CPU has AVX2 features: {CpuInformation.HasAvx2}");
+Functions.LogInformation((int)MyCategories.CpuInformation, $"CPU has AVX-512F features: {CpuInformation.HasAvx512F}");
+Functions.LogInformation((int)MyCategories.CpuInformation, $"CPU has ARM SIMD features: {CpuInformation.HasArmSimd}");
+Functions.LogInformation((int)MyCategories.CpuInformation, $"CPU has NEON features: {CpuInformation.HasNeon}");
 
 using (new Subsystem(SubsystemFlags.Video))
 {
@@ -52,5 +71,6 @@ using (new Subsystem(SubsystemFlags.Video))
 internal enum MyCategories
 {
     LibraryInformation = Category.Custom,
-    SubsystemInformation
+    SubsystemInformation,
+    CpuInformation
 }
